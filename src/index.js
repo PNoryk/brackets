312b18861ad2f0418d2910d4e43e1924ec2ee3fd
Pavel Noryk <pnoryk@gmail.com>
2021-03-15 22:53:56 +0300

    feat: add check brackets function realization

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+    let previous_start = [];
+    const starts = bracketsConfig.reduce((acc, el) => [...acc, el[0]], []);
+    if (!starts.includes(str[0])) return false;
+
+    for (const item of str) {
+        if (previous_start.length) {
+            if (
+                item ===
+                    bracketsConfig.filter(
+                        (el) =>
+                            el[0] === previous_start[previous_start.length - 1]
+                    )[0][1]
+            ) {
+                previous_start.pop();
+                continue;
+            } else if (starts.includes(item)) {
+            } else {
+                return false;
+            }
+        } else if (!starts.includes(item)) return false;
+        previous_start.push(item);
+    }
+
+    return !previous_start.length;
 }
+const config1 = [['(', ')']];
+
+console.log(check('((()))()', config1), true);
+module.exports = check;
